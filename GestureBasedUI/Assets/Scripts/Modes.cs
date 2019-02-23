39f70578cbd87f26818d8ca13dae116c62ffcf12
Trojan-Hawk <g00333333@gmit.ie>
2019-02-23 02:37:09 +0000

    Added Modes Singleton functionallity plus test cases, added .gitignore

---
+++ GestureBasedUI/Assets/Scripts/Modes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modes : MonoBehaviour {
+	// singleton design pattern  
+    private static Modes instance = null; 
+	// enum
+	public enum Mode {Menu, Create, Select}
+	public Mode mode;
+
+	void Awake() {
+		if(instance != null && instance != this) {
+			Destroy(this.gameObject);
+		} 
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	private Modes() {  
+		// set the initial mode
+		this.mode = Mode.Menu;
+	}// Constructor 
+
+    public static Modes getInstance {  
+        get {   
+			// check to see if the instance is null
+			if (instance == null){
+				// check to see if there is another instance of this class
+				instance = GameObject.FindObjectOfType<Modes>();
+				if (instance == null){
+					instance = new Modes(); 				
+				}// if
+			}  // if
+			return instance;   
+	    }// get
+    }// getInstance  
+	
+}// Modes
---
+++ GestureBasedUI/Assets/Scripts/Tests/ModesTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModesTest : MonoBehaviour {
+	public static Modes modes;
+	public static Modes modes2;
+	public bool testing;
+
+	// Use this for initialization
+	void Start () {
+		modes = Modes.getInstance;
+		testing = true;
+	}// start
+	
+	// Update is called once per frame
+	void Update () {
+		if(testing){
+			// check if it has been set initially
+			if(modes.mode == Modes.Mode.Menu) {
+				modes.mode = Modes.Mode.Create;
+				// check if it has been set correctly
+				if(modes.mode == Modes.Mode.Create) {
+					modes.mode = Modes.Mode.Select;
+					if(modes.mode == Modes.Mode.Select) {
+						// Debug.Log("TEST OK: Modes Test cases Passed.");
+						// testing = false;
+					} else {
+						Debug.Log("TEST FAIL: Value did not change from 'Create' to 'Select'.");
+					}// if/else
+				} else {
+					Debug.Log("TEST FAIL: Value did not change from 'Menu' to 'Create'.");
+				}// if/else
+			} else {
+				Debug.Log("TEST FAIL: Mode value not set initially.");
+			}// if/else
+
+			// checking if singleton is correctly working
+			modes2 = Modes.getInstance;
+			if(modes2.mode == Modes.Mode.Select) {
+				Debug.Log("TEST OK: Modes Test cases Passed.");
+				testing = false;
+			}// if
+			
+		}// if
+	}// Update
+}// ModesTest
