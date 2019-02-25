59b3418307ca7865de79862ec42bd054564deba8
Trojan-Hawk <g00333333@gmit.ie>
2019-02-25 01:19:40 +0000

    Removed Start and Update from Create, Select and Menu Mode classes

--- GestureBasedUI/Assets/Scripts/MenuMode.cs
+++ GestureBasedUI/Assets/Scripts/MenuMode.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class MenuMode : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+	
+}// MenuMode
--- GestureBasedUI/Assets/Scripts/SelectMode.cs
+++ GestureBasedUI/Assets/Scripts/SelectMode.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectMode : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+	
+
+}// SelectMode
