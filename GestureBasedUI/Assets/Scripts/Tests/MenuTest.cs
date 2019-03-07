8af159521e56e8da5820cd60e18e03a6607400d9
yrrag5 <G00335806@gmit.ie>
2019-03-07 13:34:11 +0000

    Created instance for mode specfic testing

---
+++ GestureBasedUI/Assets/Scripts/Tests/MenuTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTest : MonoBehaviour {
+    public static Modes modes;
+
+    // Use this for initialization
+    void Start () {
+        modes = Modes.getInstance;
+        modes.mode = Modes.Mode.Menu;
+        // open the lock on the runner
+        ModeRunner mr = GameObject.FindObjectOfType<ModeRunner>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        modes.mode = Modes.Mode.Menu;
+        // open the lock on the runner
+        ModeRunner mr = GameObject.FindObjectOfType<ModeRunner>();
+
+    }
+}
