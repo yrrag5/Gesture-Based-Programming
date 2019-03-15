d9ba6242b257ee43a439bb169baebb1da594b723
HUGHBRADY <G00338260@gmit.ie>
2019-03-15 17:33:43 +0000

    creating primitive objects

--- GestureBasedUI/Assets/Scripts/Tests/CreateTest.cs
+++ GestureBasedUI/Assets/Scripts/Tests/CreateTest.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateTest : MonoBehaviour {
-	public static Modes modes;
-	public static Modes modes2;
-	public bool create;
+	public static Modes currMode;
 
 	// Use this for initialization
 	void Start () {
-		modes = Modes.getInstance;
-		modes.mode = Modes.Mode.Create;
+		currMode = Modes.getInstance;
+		currMode.mode = Modes.Mode.Create;
 	}// start
 	
 	// Update is called once per frame
 	void Update () {
-		modes.mode = Modes.Mode.Create;
 
+		currMode.mode = Modes.Mode.Create;
+
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			cube.transform.position = new Vector3 (0f, 2f, -7f);
+			cube.AddComponent<Rigidbody>();
+		}
+		if (Input.GetMouseButtonDown (1)) {
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cylinder);
+			cube.transform.position = new Vector3 (0f, 2f, -7f);
+			cube.AddComponent<Rigidbody>();
+		}
+		
+	}
+
+	void Create (string prefabName) {
+		// This method recieves a string and creates the corresponding prefab. 
+		// 
+		
 	}
 }
