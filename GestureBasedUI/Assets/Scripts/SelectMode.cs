2272a089fbe27a172bab42ba65e4987907fe410c
Trojan-Hawk <g00333333@gmit.ie>
2019-03-29 18:48:25 +0000

    Edited object focus script

--- GestureBasedUI/Assets/Scripts/SelectMode.cs
+++ GestureBasedUI/Assets/Scripts/SelectMode.cs
@@ -2,7 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
 public class SelectMode : MonoBehaviour {
+	// Myo game object to connect with.
+    // This object must have a ThalmicMyo script attached.
+    public GameObject myo = null;
+	// A rotation that compensates for the Myo armband's orientation parallel to the ground, i.e. yaw.
+    // Once set, the direction the Myo armband is facing becomes "forward" within the program.
+    // Set by making the fingers spread pose or pressing "r".
+    private Quaternion _antiYaw = Quaternion.identity;
+	// A reference angle representing how the armband is rotated about the wearer's arm, i.e. roll.
+    // Set by making the fingers spread pose or pressing "r".
+    private float _referenceRoll = 0.0f;
+
+    // The pose from the last update. This is used to determine if the pose has changed
+    // so that actions are only performed upon making them rather than every frame during
+    // which they are active.
+    private Pose _lastPose = Pose.Unknown;
+
 	public static Modes modes;
 	// lock
 	private bool locked = true;
@@ -18,7 +39,8 @@ public class SelectMode : MonoBehaviour {
 		// if the class is not locked out
 		if(!locked){
 			Debug.Log("SelectMode Unlocked");
-
+			// Access the ThalmicMyo component attached to the Myo object.
+        	ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
 
 
 		}// if
@@ -36,9 +58,14 @@ public class SelectMode : MonoBehaviour {
 		LookAt lookAt = (LookAt)FindObjectOfType(typeof(LookAt));
 		// passing the selected objects transform to the LookAt script
 		lookAt.Focus(selected.transform.position);
+		
+		// Need the camera to be angled down at the object not angled up
+
 	}// focusSelected
 
 	public void CreateMode() {
+		// gets a handle on the singleton instance
+		modes = Modes.getInstance;
 		// change the mode to exit
 		modes.mode = Modes.Mode.Create;
 		// open the lock on the runner
