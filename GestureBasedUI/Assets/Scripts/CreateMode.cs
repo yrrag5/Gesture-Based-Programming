a0836d225aaf65722764b3cd3f3b20d77ad942b1
HUGHBRADY <G00338260@gmit.ie>
2019-04-04 21:41:55 +0100

    Myo gestures functions set, ToggleHighlight code added.

--- GestureBasedUI/Assets/Scripts/CreateMode.cs
+++ GestureBasedUI/Assets/Scripts/CreateMode.cs
@@ -15,6 +15,8 @@ public class CreateMode : MonoBehaviour {
 	private GameObject selected;
 	public int arrayLength;
 	public int arrayIndex;
+	public Material shapecolor;
+	public Material highlight;
 
 	// Myo variables.
 	public GameObject myo = null;
@@ -37,8 +39,10 @@ public class CreateMode : MonoBehaviour {
 		if (arrayLength != 0) {
 			Debug.Log("length: " + arrayLength);
 
-			// Set selected shape to last shape in GameObjects array.
+			// Set selected shape to last object in GameObjects array.
 			selected = sceneState.getObject(arrayLength - 1);
+			// Set array index to last object in array.
+			arrayIndex = arrayLength - 1;
 		}
 		else {
 			Debug.Log("Object array is empty");
@@ -49,43 +53,32 @@ public class CreateMode : MonoBehaviour {
 		// If the class is not locked out.
 		if(!locked) {
 			Debug.Log("CreateMode Unlocked");
-			
+
 			// Detect Myo gestures (left, right, close, exit).
 			// Access the ThalmicMyo component attached to the Myo object.
        		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
 
 			// Update references when the pose becomes fingers spread or the q key is pressed.
-			if (thalmicMyo.pose != lastPose) {
+			// if pose == whatever && != lastpose
+			if (thalmicMyo.pose == Pose.FingersSpread && thalmicMyo.pose != lastPose) {
 				lastPose = thalmicMyo.pose;
-
-				if (thalmicMyo.pose == Pose.FingersSpread) {
-					Debug.Log("create mode - Finger Spread");
-				//	MenuMode();
-				}
+				Debug.Log("create mode - Finger Spread");
+				MenuMode();
 			}
-			else if (thalmicMyo.pose != lastPose) {
+			else if (thalmicMyo.pose == Pose.WaveIn && thalmicMyo.pose != lastPose) {
 				lastPose = thalmicMyo.pose;
-				
-				if (thalmicMyo.pose == Pose.WaveIn) {
-					Debug.Log("create mode - Wave in");
-				//	ParseLeft(arrayIndex);
-				}
+				Debug.Log("create mode - Wave in");
+				ParseLeft(arrayIndex);
 			}
-			else if (thalmicMyo.pose != lastPose) {
+			else if (thalmicMyo.pose == Pose.WaveOut && thalmicMyo.pose != lastPose) {
 				lastPose = thalmicMyo.pose;
-				
-				if (thalmicMyo.pose == Pose.WaveOut) {
-					Debug.Log("create mode - Wave out");
-					ParseRight(arrayIndex);
-				}
+				Debug.Log("create mode - Wave out");
+				ParseRight(arrayIndex);
 			}
-			else if (thalmicMyo.pose != lastPose) {
+			else if (thalmicMyo.pose == Pose.DoubleTap && thalmicMyo.pose != lastPose) {
 				lastPose = thalmicMyo.pose;
-				
-				if (thalmicMyo.pose == Pose.DoubleTap) {
-					Debug.Log("create mode - Double Tap");
-				//	SelectMode();
-				}
+				Debug.Log("create mode - Double Tap");
+				SelectMode();
 			}
 		}// if
 
@@ -97,15 +90,17 @@ public class CreateMode : MonoBehaviour {
 		ToggleHighlight();
 		// Select GameObject by decrementing index of array by 1.
 		// if index == 0, select the last GameObject in array.
-		if (arrayIndex == 0) 
-			selected = sceneState.getObject(arrayLength - 1);
+		if (arrayIndex == 0) {
+			arrayIndex = arrayLength - 1;
+			selected = sceneState.getObject(arrayIndex);
+		}
 		else {
 			arrayIndex -= 1;
 			selected = sceneState.getObject(arrayIndex);
 		}
 
-		// Need code for camera.
-
+		// Focus camera on object.
+		FocusSelected();
 		// Highlight the newly selected shape.
 		ToggleHighlight();		
 	}
@@ -116,25 +111,37 @@ public class CreateMode : MonoBehaviour {
 
 		// Select GameObject by incrementing index of array by 1.
 		// if index == array.length - 1, select the first GameObject in array.
-		if (arrayIndex == arrayLength - 1) 
-			selected = sceneState.getObject(0);
+		if (arrayIndex == arrayLength - 1) {
+			arrayIndex = 0;
+			selected = sceneState.getObject(arrayIndex);
+		}
 		else {
 			arrayIndex += 1;
 			selected = sceneState.getObject(arrayIndex);
 		}
 
-		// Need code for camera.
-
+		// Focus camera on object.
+		FocusSelected();
 		// Highlight the newly selected shape.
-		ToggleHighlight();	
+		ToggleHighlight();
+	}
+
+	// Focus camera on selected object.
+	public void FocusSelected(){
+		// getting a handle on the LookAt script
+		LookAt lookAt = (LookAt)FindObjectOfType(typeof(LookAt));
+		// passing the selected objects transform to the LookAt script
+		lookAt.Focus(selected.transform.position);
 	}
 
 	void ToggleHighlight() {
-		// Toggle between highlighted and not.
-		if (selected.GetComponent<Renderer>().material.color == Color.white) 
-			selected.GetComponent<Renderer>().material.color = Color.red;
-		else
-			selected.GetComponent<Renderer>().material.color = Color.white;
+		// If material is highlighted.
+		if (selected.GetComponent<Renderer>().material == highlight)
+			selected.GetComponent<Renderer>().material = shapecolor;
+		else { // otherwise, store the material and then change to highlighted.
+			shapecolor = selected.GetComponent<Renderer>().material;
+			selected.GetComponent<Renderer>().material = highlight;
+		}
 	}
 
 	// ----- MODE CHANGING METHODS -----
@@ -161,12 +168,12 @@ public class CreateMode : MonoBehaviour {
 
 		// Gets a handle on the singleton instance.
 		modes = Modes.getInstance;
-		// Change the mode to exit
+		// Change the mode.
 		modes.mode = Modes.Mode.Select;
 		// Open the lock on the runner.
 		ModeRunner mr = GameObject.FindObjectOfType<ModeRunner>();
 		mr.Locked = false;
-		// Close the lock here.
+		// Close the lock.
 		this.locked = true;
 	}// Exit
 	
